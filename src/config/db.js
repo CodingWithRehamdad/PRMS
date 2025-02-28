287de19386b7fe8606670f72067fd764a7830c3e
CodingWithRehamdad <kzada3349@gmail.com>
2025-02-28 16:03:25 +0500

    nothing changed

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,17 @@
-require('dotenv').config({path: './src/.env'});
+require('dotenv').config({ path: require('path').join(__dirname, '../.env') });
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        console.log('MongoDB URI:', process.env.MONGODB_URI); 
+
+        if (!process.env.MONGODB_URI) {
+            throw new Error('MONGO_URI is not defined in .env file');
+        }
+
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Connected to MongoDB');
+        console.log('✅ Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);
         process.exit(1);
@@ -12,4 +19,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,2 @@
+const express = require('express')
+const { auth, roleMiddleware } = require('../middlewares/authMiddleware')
